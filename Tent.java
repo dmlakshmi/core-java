7581a5422cb8a02bc65a290cc1396a95e9d70d18
dmlakshmi <lakshmidm.xworkz@gmail.com>
2023-11-07 11:00:46 +0530

    tent

---
+++ Tent.java
@@ -0,0 +1,38 @@
+class Tent
+{
+int slno;
+Color color;
+double width;
+double height;
+boolean open()
+{
+System.out.println("tent is opened:");
+return true;
+}
+boolean close()
+{
+System.out.println("tent is closed:");
+return false;
+}
+void showInfo(int slno,Color color,double width,double height)
+{
+	this.color=color;
+	this.slno=slno;
+	this.width=width;
+	this.height=height;
+	System.out.println("Starting showing in tent:");
+    System.out.println("tent slno is"+slno);
+	if (color!=null)
+	{
+		System.out.println("tent color is"+color.RED);
+		System.out.println("tent color is"+color.BLUE);
+		System.out.println("tent color is"+color.GREEN);
+	}
+	System.out.println("tent width is "+width);
+	System.out.println("ending showInfo in tent");
+}
+}
+
+
+
+	
